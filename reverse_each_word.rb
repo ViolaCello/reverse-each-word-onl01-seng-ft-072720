c8205691a1246e4a8f95ccd130c23d8c954cebd7
Anthony Lepore <violacellonyc@gmail.com>
2020-07-29 18:14:11 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,10 @@
 def reverse_each_word(phrase)
   new_phrase = []
   a = phrase.split(" ")
-    a.each do |rev|
+    a.collect do |rev|
       new_phrase.push(rev.reverse)
     end
-  new_phrase.join(" ")
+  
 end
 
 # def reverse_each_word(phrase)
